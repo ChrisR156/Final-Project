017e6d7ac6b2b9cfe69547d697e666e56f6f5c2b
ChrisR156 <106751221+ChrisR156@users.noreply.github.com>
2022-09-21 23:43:07 +0400

    Final-Ready
    Ready

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 const menuBtn = document.querySelector('.burger-menu');
 const sliderMenu = document.querySelector('.burger-menu-slider');
 let menuOpen = false;
+
+
+const burgerNavigation = `<nav class="burger-navigation">
+					
+					<a href="#" class="divhome">HOME</a>
+					<a href="#section-about" class="divabout">ABOUT</a>
+					<a href="#section-services" class="divservices">SERVICES</a>
+					<a href="#section-activities" class="divactivities">ACTIVITIES</a>
+					<a href="#section-team" class="divteam">TEAM</a>
+					<a href="#section-contact" class="divcontact">CONTACT</a>
+
+
+
+				</nav>`
+
+
 menuBtn.addEventListener('click', () => {
 	if(!menuOpen) {
 		menuBtn.classList.add('open');
+		sliderMenu.classList.add('open')
+		sliderMenu.innerHTML = burgerNavigation;
 		menuOpen = true;
 	} else {
 		menuBtn.classList.remove('open');
+		sliderMenu.classList.remove('open')
+		sliderMenu.innerHTML = "";
 		menuOpen = false
 	
 }
